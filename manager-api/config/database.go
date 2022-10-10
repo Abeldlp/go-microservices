bb7b5500b5bde8c1b513ae50842e014e7e9c6950
Abel de la Paz <abel45991690@gmail.com>
2022-10-10 21:44:04 +0200

    test: add postgres database

--- manager-api/config/database.go
+++ manager-api/config/database.go
@@ -2,15 +2,20 @@ package config
 
 import (
 	"github.com/Abeldlp/go-and-compose/models"
-	"gorm.io/driver/mysql"
+	// "gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 var DB *gorm.DB
 
 func InitializeDatabase() {
-	dsn := "user:password@tcp(manager-api-db:3306)/manager-api?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := "user:password@tcp(manager-api-db:3306)/manager-api?charset=utf8mb4&parseTime=True&loc=Local"
+
+	dsn := "host=" + os.Getenv("DATABASE_HOST") + " user=" + os.Getenv("DATABASE_USER") + " password=" + os.Getenv("DATABASE_PASSWORD") + " dbname=" + os.Getenv("DATABASE_NAME") + " port=9920 sslmode=disable TimeZone=Europe/Amsterdam"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
--- manager-api/go.mod
+++ manager-api/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.8.1
-	gorm.io/driver/mysql v1.3.6
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/driver/postgres v1.4.4
 	gorm.io/gorm v1.23.10
 )
